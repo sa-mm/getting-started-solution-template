bcd50e2cf12e666c4f0f2461d1c7ef499cac019a
Renaud Sauvain <renaudsauvain@exosite.com>
2019-05-14 18:21:52 +0800

    Webservice endpoint tags & other doc (Issue: MUR-9396)

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -1,5 +1,7 @@
---#ENDPOINT POST /api/user
-print("Creating a new user")
+--#ENDPOINT POST /api/user text/plain
+return "new-user-id"
 
 --#ENDPOINT GET /api/user/{userId}
+--#TAGS user public
 print("Fetch a given user" .. request.parameters.userId)
+return {id=request.parameters.userId}  -- json by default
