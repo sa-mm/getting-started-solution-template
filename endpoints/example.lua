646ef3a4c875a1bd7cb6c35cafea16dd6cca5102
Lena Wu <lenawu@exosite.com>
2019-09-20 14:49:08 +0800

    Add description

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -3,6 +3,13 @@
 -- A lua handler for this endpoint, doc: http://docs.exosite.com/reference/services/webservice/#request
 return "new-user-id"
 
+--#ENDPOINT GET /api/summary/user/{userId}
+--#TAGS user public
+--#SUMMARY api to get user
+--#DESCRIPTION this is an api to get user by userId
+print("Fetch a given user" .. request.parameters.userId)
+return {id=request.parameters.userId}  -- json by default
+
 --#ENDPOINT GET /api/user/{userId}
 --#TAGS user public
 print("Fetch a given user" .. request.parameters.userId)
