d7bbab832d71004649422668a8217b12b38db9e8
Renaud Sauvain <renaudsauvain@exosite.com>
2019-11-27 15:32:15 +0800

    Add trigger endpoint

--- endpoints/c2c/callbacks.lua
+++ endpoints/c2c/callbacks.lua
@@ -1,6 +1,17 @@
 -- Configure here the callbacks logic from the remote cloud.
 -- Set the authentication logic in the authentication module.
 
+--#ENDPOINT GET /c2c/callback
+-- This endpoint enables an active notification trigger without passing the data
+local peer = require("c2c.authentication").getPeer(request)
+
+if peer == nil then
+  response.code = 401
+  return ""
+end
+
+return require("c2c.murano2cloud").syncAll()
+
 --#ENDPOINT POST /c2c/callback
 
 -- Authenticate the 3rd party request
