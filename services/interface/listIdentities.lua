e48e91288372d06c4151ecce6a36c816200950fd
Renaud Sauvain <renaudsauvain@exosite.com>
2019-11-28 14:39:20 +0800

    ConfigIo passing tweek

--- services/interface/listIdentities.lua
+++ services/interface/listIdentities.lua
@@ -1,17 +1,18 @@
 operation.solution_id = nil
 local identities = Device2.listIdentities(operation)
-if identities.code ~= nil then
+if identities.error then
   return identities
 end
 
 local configIO = require("vendor.configIO")
-if configIO and next(identities.devices) ~= nil then
+if configIO and identities.devices and next(identities.devices) ~= nil then
+  local config_io = {
+    timestamp = configIO.timestamp,
+    set = configIO.config_io,
+    reported = configIO.config_io
+  }
   for k, identity in pairs(identities.devices) do
-    identities.devices[k].state.config_io = {
-      timestamp = configIO.timestamp,
-      set = configIO.config_io,
-      reported = configIO.config_io
-    }
+    identities.devices[k].state.config_io = config_io
   end
 end
 
