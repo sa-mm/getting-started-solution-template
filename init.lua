7a6a236374a73a57e98ec6ca9909e363f96663f3
Tristan Harling <tristanharling@exosite.com>
2020-01-10 15:33:14 +0800

    first merge

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
--- init script content
-print("All set")
+local i = require("pdaas.interface")
+local m = require("pdaas.lib.migrations")
+local version, error = m.run()
+if error ~= nil then
+  log.error("Migration failed with: " .. error)
+else
+  log.warn("Migration succeeded, current version: " .. tostring(version))
+end
+i.updateDescription()
