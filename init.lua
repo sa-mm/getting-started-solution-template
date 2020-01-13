4a901caba523a7298365e6235f3ed956d202f42b
Tristan Harling <tristanharling@exosite.com>
2020-01-13 15:35:28 +0800

    added merge

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 
 -- Generate callbackUrl
 require("c2c.authentication").setToken()
+local i = require("pdaas.interface")
+local m = require("pdaas.lib.migrations")
+local version, error = m.run()
+if error ~= nil then
+  log.error("Migration failed with: " .. error)
+else
+  log.warn("Migration succeeded, current version: " .. tostring(version))
+end
+i.updateDescription()
